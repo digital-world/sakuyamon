6e6a5a76cee14bbb06d0a66c64504d5f9e8fa542
WarGrey Gyoudmon Ju <juzhenliang@gmail.com>
2014-12-20 15:18:49 +0800

    Update Mission: README.md: Define island/* conventions for web application

--- info.rkt
+++ info.rkt
@@ -5,3 +5,14 @@
 (define version "Baby I")
 
 (define pkg-desc "Be in charge of [gyoudmon.org](http://gyoudmon.org).")
+
+(define skymn-config-port #false)
+(define skymn-config-ssl #false)
+(define skymn-config-max-waiting 511)
+
+(define skymn-timeout-initial-connection 30)
+(define skymn-timeout-default 30)
+(define skymn-timeout-password 300)
+(define skymn-timeout-servlet 86400)
+(define skymn-timeout-file/byte 1/20)
+(define skymn-timeout-file 30)
