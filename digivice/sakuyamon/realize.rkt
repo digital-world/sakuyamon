090c4365614c6c36b7a60d64ed0264d759dbce84
WarGrey Gyoudmon Ju <juzhenliang@gmail.com>
2015-04-16 04:27:43 +0800

    Update digivice: realize.rkt: launch server in a more robust way

--- digivice/sakuyamon/realize.rkt
+++ digivice/sakuyamon/realize.rkt
@@ -6,21 +6,29 @@
 
 {module sakuyamon racket/base
   (require racket/unit)
+  (require racket/async-channel)
+  
   (require net/tcp-sig)
-  (require (only-in web-server/web-server do-not-return))
   (require web-server/private/dispatch-server-sig)
   (require web-server/private/dispatch-server-unit)
 
-  ;(require "../../digitama/configuration.rkt")
-
+  (require "../../digitama/configuration.rkt")
   
-      #|(define-compound-unit sakuyamon@
-      (import)
-      (export DS)
-      (link [{{DS : dispatch-server^}} dispatch-server@ T DSC]
-            [{{T : tcp^}} sakuyamon-tcp@ DS]
-            [{{DSC : dispatch-server-config^}} sakuyamon-config@ DS]))
+  (define-compound-unit sakuyamon@
+    (import)
+    (export DS)
+    (link [{{DS : dispatch-server^}} dispatch-server@ T DSC]
+          [{{T : tcp^}} sakuyamon-tcp@ DS]
+          [{{DSC : dispatch-server-config^}} sakuyamon-config@ DS]))
+  (define-values/invoke-unit/infer sakuyamon@)
 
-    (define-values/invoke-unit/infer sakuyamon@)
-    (void (serve))
-    (do-not-return)|# (displayln (current-command-line-arguments))}
+  (let* ([ping (make-async-channel #false)]
+         [shutdown (parameterize ([error-display-handler void])
+                     (serve #:confirmation-channel ping))]
+         [pinged (async-channel-get ping)])
+    (cond [(exn? pinged) (printf "~a~n" (exn-message pinged))] 
+          [else (dynamic-wind {λ _ (printf "sakuyamon: mission start~n")}
+                              {λ _ (with-handlers ([exn:break? {λ _ (newline)}])
+                                     #|do-not-return|# (async-channel-get ping))}
+                              {λ _ (printf "sakuyamon: mission complete!~n")})])
+    (void (shutdown)))}
