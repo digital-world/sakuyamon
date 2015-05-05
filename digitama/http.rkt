023ed6e77c22b35b5f1797313ebac0d4b7d7fab6
WarGrey Gyoudmon Ju <juzhenliang@gmail.com>
2015-05-05 08:10:29 +0800

    Add tamer: rcacket.rkt: detect status of features and bugs of Racket itself

---
+++ digitama/http.rkt
@@ -0,0 +1,52 @@
+#lang typed/racket
+
+(require typed/net/url)
+
+(provide (all-defined-out))
+(provide (all-from-out typed/net/url))
+
+(define-type Header header)
+(define-type Binding binding)
+(define-type Request request)
+(define-type Response response)
+
+(require/typed/provide web-server/http
+                       [#:struct header {[field : Bytes]
+                                         [value : Bytes]}
+                                 #:extra-constructor-name make-header]
+                       [#:struct binding {[id : Bytes]}
+                                 #:extra-constructor-name make-binding]
+                       [#:struct {binding:form binding} {[value : Bytes]}
+                                 #:extra-constructor-name make-binding:form]
+                       [#:struct {binding:file binding} {[filename : Bytes]
+                                                         [headers : (Listof Header)]
+                                                         [content : Bytes]}
+                                 #:extra-constructor-name make-binding:file]
+                       [#:struct request {[method : Bytes]
+                                          [uri : URL]
+                                          [headers/raw : (Listof Header)]
+                                          [bindings/raw-promise : (Promise (Listof Binding))]
+                                          [post-data/raw : (Option Bytes)]
+                                          [host-ip : String]
+                                          [host-port : Natural]
+                                          [client-ip : String]}
+                                 #:extra-constructor-name make-request]
+                       [#:struct response {[code : Natural]
+                                           [message : Bytes]
+                                           [seconds : Natural]
+                                           [mime : (Option Bytes)]
+                                           [headers : (Listof Header)]
+                                           [output : (-> Output-Port Void)]}])
+
+(require/typed/provide web-server/configuration/responders
+                       [file-response (-> Natural Bytes Path-String (Listof Header) Response)]
+                       [servlet-loading-responder (-> URL exn Response)]
+                       [gen-servlet-not-found (-> Path-String (-> URL Response))]
+                       [servlet-error-responder (-> URL exn Response)]
+                       [gen-servlet-responder (-> Path-String (-> URL exn Response))]
+                       [gen-servlets-refreshed (-> Path-String (-> Response))]
+                       [gen-passwords-refreshed (-> Path-String (-> Response))]
+                       [gen-authentication-responder (-> Path-String (-> URL Header Response))]
+                       [gen-protocol-responder (-> Path-String (-> URL Response))]
+                       [gen-file-not-found-responder (-> Path-String (-> Request Response))]
+                       [gen-collect-garbage-responder (-> Path-String (-> Response))])
--- tamer/handbook.scrbl
+++ tamer/handbook.scrbl
@@ -6,9 +6,25 @@
 
 @margin-note{@deftech{Sakuyamon} is the Queen of @hyperlink["http://gyoudmon.org"]{gyoudmon.org}.}
 
-I am sorry meanwhile I could not tell more details.
+Please do not ask me why I choose @hyperlink["http://www.racket-lang.org"]{@bold{Racket}} to build my website.
+Meanwhile I don@literal{'}t know more than you about whether it is suitable for this kind of tasks since its
+real world applications are damn harder to be found than heard of. So you get it, I mean I like it and just
+for fun, maybe also paid for it.
+
+Due to my poor knowledge on @bold{Racket}, infrasturctures are designed as lightweight as possible. Instances
+include almost all batteries with zero configuration, and they should communicate with each other easily. Keep it
+simple but also ready for large-scale extension.
+
+Each instance has 3 types of @deftech[#:key "terminus"]{termini}, or @italic{htdocs} or @italic{webroot}:
+@(itemlist #:style 'compact
+           @item{@deftech{@bold{Main Terminus}}: The major and default one shipped with the instance.}
+           @item{@deftech{@bold{Per-User Terminus}}: The one made for system users in their home directories.}
+           @item{@deftech{@bold{Per-Digimon Terminus}}: The one made for project wikis like
+                  @hyperlink["https://help.github.com/articles/what-are-github-pages/"]{Github Pages}.})
 
 @tamer-smart-summary[]
 @handbook-smart-table[]
 
 @include-section[(submod "sakuyamon.rkt" doc)]
+
+@include-section[(submod "racket.rkt" doc)]
