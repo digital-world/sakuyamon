e35d07cf6efd46757950b5702b5b291ef0e77dd7
WarGrey Gyoudmon Ju <juzhenliang@gmail.com>
2015-05-22 20:39:44 +0800

    Update Mission: submake.rkt: restart service after making

--- submake.rkt
+++ submake.rkt
@@ -5,32 +5,40 @@
 (require syntax/location)
 (require make)
 
-{module+ make:files
-  (define sakuyamon.plist "/System/Library/LaunchDaemons/org.gyoudmon.sakuyamon.plist")
-  (define sakuyamon.sh "/etc/init.d/sakuyamon")
+(define sakuyamon.plist "/System/Library/LaunchDaemons/org.gyoudmon.sakuyamon.plist")
+(define sakuyamon.sh "/etc/init.d/sakuyamon")
 
-  (define /stone/launchd.plist (build-path (digimon-stone) "launchd.plist"))
-  (define /stone/initd.sh (build-path (digimon-stone) "initd.sh"))
+(define /stone/launchd.plist (build-path (digimon-stone) "launchd.plist"))
+(define /stone/initd.sh (build-path (digimon-stone) "initd.sh"))
+
+{module+ premake
+  (define sudo.make {λ [dest src] (with-output-to-file dest #:exists 'replace
+                                    {λ _ (dynamic-require src #false)})})
   
-  {module+ make
-    (define sudo.make {λ [dest src] (with-output-to-file dest #:exists 'replace
-                                      {λ _ (dynamic-require src #false)})})
+  (when (string=? (getenv "USER") "root")
+    (or (system (format "sh ~a ~a create" (build-path (digimon-stone) "tamer.sh") (system-type 'os)))
+        (error 'make "Failed to separate privileges!"))
     
-    (when (string=? (getenv "USER") "root")
-      (or (system (format "sh ~a ~a create" (build-path (digimon-stone) "tamer.sh") (system-type 'os)))
-          (error 'make "Failed to separate privileges!"))
+    (make ([sakuyamon.plist [/stone/launchd.plist (quote-source-file)] (void (sudo.make sakuyamon.plist /stone/launchd.plist)
+                                                                             (system (format "chown root:wheel ~a" sakuyamon.plist)))]
+           [sakuyamon.sh [/stone/initd.sh (quote-source-file)] (void (sudo.make sakuyamon.sh /stone/initd.sh)
+                                                                     (system (format "chown root:root ~a" sakuyamon.sh)))])
+          (list (case (system-type 'os)
+                  [{macosx} sakuyamon.plist]
+                  [{unix} sakuyamon.sh]))))}
 
-      (make ([sakuyamon.plist [/stone/launchd.plist (quote-source-file)] (void (sudo.make sakuyamon.plist /stone/launchd.plist)
-                                                                               (system (format "chown root:wheel ~a" sakuyamon.plist)))]
-             [sakuyamon.sh [/stone/initd.sh (quote-source-file)] (void (sudo.make sakuyamon.sh /stone/initd.sh)
-                                                                       (system (format "chown root:root ~a" sakuyamon.sh)))])
-            (list (case (system-type 'os)
-                    [{macosx} sakuyamon.plist]
-                    [{unix} sakuyamon.sh]))))}
-
+{module+ make:files 
   {module+ clobber
     (when (string=? (getenv "USER") "root")
       (system (format "sh ~a ~a delete" (build-path (digimon-stone) "tamer.sh") (system-type 'os)))
       (delete-file (case (system-type 'os)
                      [{macosx} sakuyamon.plist]
                      [{unix} sakuyamon.sh])))}}
+
+{module+ postmake
+  (when (string=? (getenv "USER") "root")
+    (case (system-type 'os)
+      [{macosx} (and (system (format "rm -fr ~a ~a" (build-path (digimon-stone) "stderr.log") (build-path (digimon-stone) "stdout.log")))
+                     (system (format "launchctl unload ~a" sakuyamon.plist))
+                     (system (format "launchctl load ~a" sakuyamon.plist)))]
+      [{unix} (system (format "sh ~a restart" sakuyamon.sh))]))}
