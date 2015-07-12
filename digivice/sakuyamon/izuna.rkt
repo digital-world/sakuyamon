e0d5003c2b91658f34e7adbbe13ace764e0259d1
WarGrey Gyoudmon Ju <juzhenliang@gmail.com>
2015-07-12 18:36:19 +0800

    Update digivice: izuna.rkt: colorize log messages

--- digivice/sakuyamon/izuna.rkt
+++ digivice/sakuyamon/izuna.rkt
@@ -6,6 +6,7 @@
 
 (module+ sakuyamon
   (require/typed racket/gui/dynamic
+                 [gui-available? (-> Boolean)]
                  [gui-dynamic-require (-> Symbol Any)])
 
   (require/typed racket
@@ -20,7 +21,7 @@
                                          String
                                          Index
                                          TCP-Port
-                                         [#:password (Option String)]
+                                         [#:username String]
                                          [#:id_rsa.pub Path-String]
                                          [#:id_rsa Path-String]
                                          [#:passphrase String]
@@ -29,26 +30,37 @@
   (define sakuyamon-scepter-host : (Parameterof String) (make-parameter "gyoudmon.org"))
   (define sakuyamon-scepter-port : (Parameterof Index) (make-parameter (sakuyamon-foxpipe-port)))
 
-  (define main : (-> Any)
+  (define filter-message : (-> String String)
+    (lambda [msg]
+      msg))
+
+  (define cli-main : (-> Any)
     (thunk (parameterize ([current-custodian (make-custodian)])
              (define izunac : Thread (current-thread))
              (define sshc : (Parameterof (Option Thread)) (make-parameter #false))
+             (define colors : (Listof Term-Color) (list 159 191 223 255))
              (with-handlers ([exn:break? (lambda [[signal : exn]] (let ([t (sshc)]) (when (thread? t) (break-thread t))))])
-               (define-values [/dev/sshin /dev/sshout] (tcp-connect (cast (sakuyamon-scepter-host) String) 22))
-               (printf "connected to ~a:~a.~n" (sakuyamon-scepter-host) 22)
-               (sshc (thread (thunk (sakuyamon-foxpipe izunac "localhost" (sakuyamon-scepter-port) (cast /dev/sshout TCP-Port)))))
-               (let poll ()
-                 (match (sync/timeout/enable-break (* (sakuyamon-foxpipe-idle) 1.618)
-                                                   (wrap-evt (thread-dead-evt (cast (sshc) Thread)) (const 'collapsed))
-                                                   (wrap-evt (thread-receive-evt) (lambda [e] (thread-receive))))
-                   [#false (let ([t (cast (sshc) Thread)]) (unless (thread-dead? t) (break-thread t)))]
-                   ['collapsed (eprintf "tunnel has collapsed: ~a~n" (thread-receive))]
-                   [(cons figureprint authlist) (printf "~a~n~a~n" figureprint authlist)]
-                   [(? box? msgbox) (printf "~a~n" (unbox msgbox))]
-                   [(? exn? exception) (displayln exception)]
-                   [event (printf "Uncaught Event: ~a~n" event)])
-                 (unless (thread-dead? (cast (sshc) Thread)) (poll)))))))
-  
+               (let re-connect ()
+                 (define-values [/dev/sshin /dev/sshout] (tcp-connect (cast (sakuyamon-scepter-host) String) 22))
+                 (echof #:fgcolor 'blue "connected to ~a:~a.~n" (sakuyamon-scepter-host) 22)
+                 (sshc (thread (thunk (sakuyamon-foxpipe izunac "localhost" (sakuyamon-scepter-port) (cast /dev/sshout TCP-Port)))))
+                 (let poll ([index : Index 0])
+                   (match (sync/timeout/enable-break (* (sakuyamon-foxpipe-idle) 1.618)
+                                                     (wrap-evt (thread-dead-evt (cast (sshc) Thread)) (const 'collapsed))
+                                                     (wrap-evt (thread-receive-evt) (lambda [e] (thread-receive))))
+                     [#false (let ([t (cast (sshc) Thread)])
+                               (eechof #:fgcolor 'red "connection is lost!~n")
+                               (unless (thread-dead? t) (break-thread t)))]
+                     ['collapsed (eechof #:fgcolor 'red "tunnel has collapsed: ~a~n" (thread-receive))]
+                     [(cons figureprint authlist) (echof #:fgcolor 'cyan "figureprint: ~a~nauthentication: ~a~n" figureprint authlist)]
+                     [(? box? msgbox) (let ([msg (cast (unbox msgbox) String)])
+                                        (unless (string=? msg (string beating-heart#))
+                                          (echof #:fgcolor (list-ref colors index) "~a~n" (filter-message msg))))]
+                     [(? exn? exception) (echof #:fgcolor 'red "~a~n" (exn-message exception))]
+                     [event (echof #:fgcolor 'yellow "Uncaught Event: ~a~n" event)])
+                   (cond [(thread-dead? (cast (sshc) Thread)) (re-connect)]
+                         [else (poll (remainder (add1 index) (length colors)))])))))))
+
   (call-as-normal-termination
    (thunk (parameterize ([current-directory (find-system-path 'orig-dir)])
             ((cast parse-command-line (-> String (Vectorof String) Help-Table (-> Any String Any)
@@ -59,7 +71,9 @@
                [once-each
                 [{"-p"} ,(λ [[flag : String] [port : String]] (sakuyamon-scepter-port (cast (string->number port) Index)))
                         {"Use an alternative <port>." "port"}]])
-             (lambda [!flag hostname] (and (sakuyamon-scepter-host hostname) (main)))
+             (lambda [!flag hostname] (and (sakuyamon-scepter-host hostname)
+                                           (cond [(gui-available?) (cli-main)]
+                                                 [else (cli-main)])))
              '{"hostname"}
              (lambda [[-h : String]]
                (display (string-replace -h #px"  -- : .+?-h --'\\s*" ""))
