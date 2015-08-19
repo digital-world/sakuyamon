2c7d9615692de47fc7ee07ef6ad7d6c52cf771ba
WarGrey Gyoudmon Ju <juzhenliang@gmail.com>
2015-08-20 00:10:46 +0800

    Update digitama: termctl.rkt: display colors with the right window/pad API

--- digitama/termctl.rkt
+++ digitama/termctl.rkt
@@ -13,10 +13,14 @@
            #:global? #true))
 
 (define _window* (_cpointer/null 'WINDOW*))
-(define _winpad* (_cpointer/null 'WINDOW*))
 (define _ok/err (make-ctype _int #false (lambda [c] (not (eq? c -1)))))
 
-(define _chtype (make-ctype _int char->integer integer->char))
+(define _chtype
+  (let ([mask (c-extern 'CHARTEXT)])
+    ;;; although char is combined with attribute and color, use (wcolor_set) and (wattr_on) instead
+    (make-ctype _uint
+                (lambda [r] (bitwise-and (char->integer r) mask))
+                (lambda [c] (integer->char (bitwise-and c mask))))))
 
 (define-values [_attr _color/term]
   (let*-values ([[symbol-downcase] (compose1 string->symbol string-downcase symbol->string)]
@@ -25,12 +29,14 @@
                                         (list 'none '= -1) (list 'BLACK 'RED 'GREEN 'YELLOW 'BLUE 'MAGENTA 'CYAN 'WHITE)))]
                 [[named-color->c c->named-color] (values (ctype-scheme->c _named-color) (ctype-c->scheme _named-color))])
     (values ((lambda [a] (_bitmask a _uint))
-             (foldl (lambda [a A] (list* (symbol-downcase a) '= (c-extern a #:ctype _uint) A)) null
-                    (list 'NORMAL 'STANDOUT 'UNDERLINE 'REVERSE 'BLINK 'DIM 'BOLD
-                          'INVIS 'PROTECT 'ALTCHARSET 'ATTRIBUTES 'CHARTEXT)))
+             (foldl (lambda [a A] (list* (symbol-downcase a) '= (c-extern a #:ctype _uint) A))
+                    null (list 'NORMAL 'STANDOUT 'UNDERLINE 'REVERSE 'BLINK 'DIM 'BOLD
+                               'INVIS 'PROTECT 'ALTCHARSET 'ATTRIBUTES 'CHARTEXT)))
             (make-ctype _short
                         (lambda [r] (if (integer? r) r (named-color->c r)))
                         (lambda [c] (if (< c 8) (c->named-color c) c))))))
+
+(define color-number (ctype-scheme->c _color/term))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;;                         WARNING: ncurses uses YX-Coordinate System                          ;;;
@@ -45,20 +51,32 @@
 (define-ncurses keypad (_fun _window* _bool -> _ok/err))
 (define-ncurses idlok (_fun _window* _bool -> _ok/err))
 (define-ncurses scrollok (_fun _window* _bool -> _ok/err))
+(define-ncurses delwin (_fun _window* -> _ok/err)) ; subwindows should be removed before removing their parent
 (define-ncurses endwin (_fun -> _ok/err))
 
-;;; Window, SubWindow/Pad and Screen functions
+;;; Window, SubWindow/Pad and Input/Output functions
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;; For the sake of simplicity, I only deal with real windows.                    ;;;
+;;;                                                                               ;;;
+;;; (newwin 0 0 0 0) return a new full-screen window                              ;;;
+;;; The derivated window is just a specific portion of its parent, they share the ;;;
+;;; output buffer, so use it to save yourself from having to do a lot of complex  ;;;
+;;; cursor-positioning math.                                                      ;;;
+;;; Pad and subpad are interesting, but I think the best way to dealing with pads ;;;
+;;; is managing the contexts with Racket since pads are just memory buffers that  ;;;
+;;; waiting for rendering but won't save you from computing draw-area reactangle  ;;;
+;;; position and size. Also pads cannot hold non-text data structure.             ;;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+(define-ncurses newwin (_fun [nlines : _int] [ncols : _int] [beginy : _int] [beginx : _int] -> _window*))
+(define-ncurses derwin (_fun [parent : _window*] [nlines : _int] [ncols : _int] [beginy : _int] [beginx : _int] -> _window*))
+(define-ncurses mvderwin (_fun _window* [beginy : _int] [beginx : _int] -> _ok/err))
+(define-ncurses mvwvline (_fun _window* [beginy : _int] [beginx : _int] [sym : _chtype = #\|] [maxlong : _int] -> _ok/err))
+(define-ncurses mvwhline (_fun _window* [beginy : _int] [beginx : _int] [sym : _chtype = #\-] [maxlong : _int] -> _ok/err))
+
 (define-ncurses getcury (_fun _window* -> _int))
 (define-ncurses getcurx (_fun _window* -> _int))
 (define-ncurses getmaxy (_fun _window* -> _int))
 (define-ncurses getmaxx (_fun _window* -> _int))
-
-(define-ncurses newwin (_fun [nlines : _int] [ncols : _int] [beginy : _int] [beginx : _int] -> _window*))
-
-(define-ncurses box (_fun _window* _chtype _chtype -> _ok/err))
-(define-ncurses wborder (_fun _window* _chtype _chtype _chtype _chtype _chtype _chtype _chtype _chtype -> _ok/err))
-(define-ncurses wvline (_fun _window* _chtype _int -> _ok/err))
-(define-ncurses whline (_fun _window* _chtype _int -> _ok/err))
 
 (define-ncurses putwin (_fun _path -> _ok/err))
 (define-ncurses getwin (_fun _path -> _ok/err))
@@ -71,50 +89,20 @@
                               [overlay : _bool]
                               -> _ok/err))
 
-(define-ncurses scr_dump (_fun _file -> _ok/err))
-(define-ncurses scr_restore (_fun _file -> _ok/err))
-(define-ncurses scr_init (_fun _file -> _ok/err))
-(define-ncurses scr_set (_fun _file -> _ok/err))
-
-(define-ncurses newpad (_fun [nlines : _int] [ncols : _int] -> _winpad*))
-(define-ncurses subpad (_fun [orig : _window*]
-                             [nlines : _int] [ncols : _int]
-                             [beginy : _int] [beginx : _int]
-                             -> _winpad*))
-
-;;; Output functions
+(define-ncurses wtimeout (_fun _window* _int -> _ok/err)) ;;; blocking mode for getch()
+(define-ncurses wgetch (_fun _window* -> [key : _int] -> (cond [(negative? key) #false] [else (integer->char key)])))
 (define-ncurses wmove (_fun _window* _int _int -> _ok/err))
-(define-ncurses waddstr (_fun _window* _string -> _ok/err))
+(define-ncurses waddwstr (_fun _window* _string/ucs-4 -> _ok/err))
 (define-ncurses mvwaddwstr (_fun _window* _int _int _string/ucs-4 -> _ok/err))
 (define-ncurses wrefresh (_fun _window* -> _ok/err))
 (define-ncurses wnoutrefresh (_fun _window* -> _ok/err))
 (define-ncurses wredrawln (_fun _window* [begin : _int] [n : _int] -> _ok/err))
 (define-ncurses doupdate (_fun -> _ok/err))
 
-(define-ncurses prefresh (_fun [pad : _winpad*] ;;; not _window*
-                               [pminrow : _int] [pmincol : _int]
-                               [sminrow : _int] [smincol : _int]
-                               [smaxrow : _int] [smaxcol : _int]
-                               -> _ok/err))
-
-(define-ncurses pnoutrefresh (_fun [pad : _winpad*] ;;; not _window*
-                                   [pminrow : _int] [pmincol : _int]
-                                   [sminrow : _int] [smincol : _int]
-                                   [smaxrow : _int] [smaxcol : _int]
-                                   -> _ok/err))
-
-;;; Input functions
-(define-ncurses wtimeout (_fun _window* _int -> _ok/err)) ;;; blocking mode for getch()
-(define-ncurses getch (_fun -> [ascii : _int]
-                            -> (with-handlers ([exn? (const #false)])
-                                 (integer->char ascii))))
-
 ;;; Color and Attribute functions
 (define-ncurses has_colors (_fun -> _bool))
 (define-ncurses use_default_colors (_fun -> _ok/err))
 (define-ncurses assume_default_colors (_fun _color/term _color/term -> _ok/err))
-
-(define color-number (ctype-scheme->c _color/term))
 
 (define-ncurses mvwchgat
   (_fun [win : _window*]
@@ -177,38 +165,50 @@
 (define-ncurses can_change_color (_fun -> _bool -> #false))
 
 ;;; Miscellaneous
+(define-ncurses curs_set (_fun _int -> _int))
 (define-ncurses def_prog_mode (_fun -> _int -> #true))
 (define-ncurses reset_prog_mode (_fun -> _int -> #true))
 (define-ncurses def_shell_mode (_fun -> _int -> #true))
 (define-ncurses reset_shell_mode (_fun -> _int -> #true))
 
+(define-ncurses scr_dump (_fun _file -> _ok/err))
+(define-ncurses scr_restore (_fun _file -> _ok/err))
+(define-ncurses scr_init (_fun _file -> _ok/err))
+(define-ncurses scr_set (_fun _file -> _ok/err))
 
 
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 (module+ test
   (provide (all-defined-out))
 
-  (define stdscr (initscr))
-  (void ((curry plumber-add-flush! (current-plumber))
-         (lambda [this]
-           (plumber-flush-handle-remove! this)
-           (wrefresh stdscr)
-           (getch)
-           (endwin)))
-
-        (with-handlers ([exn:fail? (lambda [ef] (and (endwin) (displayln (exn-message ef))))])
-          (when (and stdscr (raw) (noecho) (wtimeout stdscr -1) (keypad stdscr #true)
-                     (idlok stdscr #true) (scrollok stdscr #true))
-            (when (has_colors)
-              (start_color)
-              (use_default_colors)
-              (mvwaddwstr stdscr (getcury stdscr) (getcurx stdscr) (format "256 colors[default: ~a]~n~n" (pair_content 0)))
-              (for ([c (in-range (sub1 (c-extern 'COLORS)))])
-                (define p (add1 c))
-                (init_pair p c 'none)
-                (wcolor_set stdscr p)
-                (mvwaddwstr stdscr (getcury stdscr) (max 8 (getcurx stdscr)) (~a c #:min-width 4))
-                (when (zero? (remainder p 16)) (mvwaddwstr stdscr (getcury stdscr) (getcurx stdscr) "\n"))
-                (wstandend stdscr))
-              (mvwaddwstr stdscr (getcury stdscr) (getcurx stdscr) (format "~n~nPress any key to exit!"))
-              (mvwchgat stdscr (getcury stdscr) 0 -1 (list 'reverse) 0))))))
-
+  (when (and (initscr) (has_colors))
+    (start_color)
+    (use_default_colors)
+    (curs_set 0)
+    (define-values [field-size field-count columns lines colors] (values 4 16 (c-extern 'COLS) (c-extern 'LINES) (c-extern 'COLORS)))
+    (define-values [stdclr-cols stdclr-rows] (values (* field-count field-size) (ceiling (/ colors field-count))))
+    (define stdscr (newwin 0 0 0 0)) ; full screen window
+    (define stdclr (derwin stdscr stdclr-rows stdclr-cols 0 0))
+    (void ((curry plumber-add-flush! (current-plumber))
+           (lambda [this]
+             (plumber-flush-handle-remove! this)
+             (wrefresh stdscr)
+             ;(wrefresh stdclr) when stdclr is created via (newwin) in which cast it must be rendered after rendering stdscr
+             (wgetch stdscr)
+             (delwin stdclr) ; only if stdclr is created via (derwin) because parent always does not know its children.
+             (endwin)))
+          
+          (with-handlers ([exn:fail? (lambda [ef] (and (delwin stdclr) (endwin) (displayln (exn-message ef))))])
+            (when (and stdscr stdclr (raw) (noecho) (wtimeout stdscr -1) (keypad stdscr #true) (idlok stdscr #true) (scrollok stdscr #true))
+              (mvwaddwstr stdscr (getcury stdscr) (getcurx stdscr) (format "256 colors[default: ~a]" (pair_content 0)))
+              (mvderwin stdclr (add1 (quotient (- lines stdclr-rows) 2)) (quotient (- columns stdclr-cols) 2))
+              (for ([color-index (in-range (sub1 colors))])
+                (define pair-index (add1 color-index))
+                (init_pair pair-index 'none color-index)
+                (wcolor_set stdclr pair-index)
+                (waddwstr stdclr (~a color-index #:min-width field-size)) ; #\newline is not neccessary since the width has already been fitted 
+                (wstandend stdclr))
+              (mvwaddwstr stdscr (getcury stdscr) (getcurx stdscr) (format "~nPress any key to exit!"))
+              (mvwchgat stdscr (getcury stdscr) 0 -1 (list 'underline) 0)
+              (wstandend stdscr))))))
